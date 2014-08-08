2f951a4355e63f64db672b21cb5e7e63afa96c3b
wnuraeni <taniarida@gmail.com>
2014-08-08 13:05:39 +0700

    Database connection

---
+++ admin/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+mysql_connect('localhost','root','') or die(mysql_error());
+mysql_select_db('spk') or die(mysql_error());
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
