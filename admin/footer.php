9131bb9012f1c3527b68d0a817b3d9c28bd2f3b4
wnuraeni <taniarida@gmail.com>
2014-08-08 13:03:37 +0700

    Footer

---
+++ admin/footer.php
@@ -0,0 +1,28 @@
+ </div><!-- container in-->
+          
+        </div><!-- container out-->
+
+        <footer class="footer">
+             <div class="container">
+                 
+                 © 2013 SPKAndroid • All Rights Reserved
+                 
+             </div>
+         </footer>       
+    </body>
+</html>
+<style>
+    .wrap{
+        -moz-box-shadow: 
+            15px 0 15px -1px #ccc,
+            -15px 0 15px -1px #ccc;
+        -webkit-box-shadow: 
+            15px 0 15px -1px #ccc,
+            -15px 0 15px -1px #ccc;
+        box-shadow: 
+            15px 0 15px -1px #ccc,
+            -15px 0 15px -1px #ccc;
+        height: 100%;
+       
+    }
+</style>
