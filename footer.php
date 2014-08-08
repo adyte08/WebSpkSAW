ece71ce4c56262508a879181909eab54fa6056ce
wnuraeni <taniarida@gmail.com>
2014-08-08 13:07:56 +0700

    Footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+</div>
+         <div class="clearfix"></div>
+         
+      </div><!-- container -->
+      <footer class="footer"  style="background-color: #E0E0E0">
+             <div class="container">
+                 
+                 © 2013 SPKAndroid • All Rights Reserved
+                 
+             </div>
+         </footer>
+    </body>
+</html>
